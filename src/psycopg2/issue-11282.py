7fb0ddeeb98066f84c973baa044fe9c517507417
Mike Zimin <mikeziminio@gmail.com>
2024-01-22 01:31:16 +0400

    issue-11282

--- src/psycopg2/issue-11282.py
+++ src/psycopg2/issue-11282.py
@@ -1,8 +1,15 @@
 import psycopg2
-from psycopg2.extras import LogicalReplicationConnection
+from psycopg2._psycopg import cursor, ReplicationCursor as _psycopg_ReplicationCursor, ReplicationConnection
+from psycopg2.extras import LogicalReplicationConnection, ReplicationCursor
 
-# LogicalReplicationConnection
-connection = psycopg2.connect(connection_factory=LogicalReplicationConnection)
 
-# cursor
-cur = connection._cursor()
+def some():
+    # LogicalReplicationConnection
+    connection = psycopg2.connect(connection_factory=ReplicationConnection)
+
+    # cursor
+    cur = connection.cursor()
+
+
+print(issubclass(ReplicationCursor, cursor))
+print(issubclass(_psycopg_ReplicationCursor, cursor))
